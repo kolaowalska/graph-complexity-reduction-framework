70f9be39c366c3ec492252e4029f14b2f86c4817
KOLA <normalnie.kowalska@gmail.com>
2026-04-30 13:57:52 +0200

    add reduced_graph_key field to the returned DTO for experiments

--- src/interfaces/api.py
+++ src/interfaces/api.py
@@ -57,3 +57,7 @@ class ExperimentFacade:
             }
         except Exception as e:
             return {"status": "error", "message": str(e)}
+
+    @property
+    def service(self):
+        return self._service
